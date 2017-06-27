3369d0d9822212b228ccc150b6936463918908bc
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2017-06-27 14:26:05 +0200

    training

--- simple.py
+++ simple.py
@@ -6,7 +6,7 @@ import string
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-
+import random
 
 def findFiles(path):
     return glob.glob(path)
@@ -95,3 +95,45 @@ class RNN(nn.Module):
 n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
 
+
+def categoryFromOutput(output):
+    top_n, top_i = output.data.topk(1)
+    category_i = top_i[0][0]
+    return all_categories[category_i], category_i
+
+
+def randomChoice(list):
+    return list[random.randint(0, len(list) - 1)]
+
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
+    line_tensor = Variable(lineToTensor(line))
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    print('category =', category, '/ line =', line)
+
+
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+    rnn.zero_grad()
+
+    # For every character in line
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    for p in rnn.parameters():
+        p.data_add_(-learning_rate, p.grad.data)
+
+    return output, loss.data[0]
