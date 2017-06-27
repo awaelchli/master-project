4a9ce522386fd0ea2f490fce9d66565ad90d965a
awaelchli <adrian.waelchli@students.unibe.ch>
2017-06-27 12:35:21 +0200

    rnn model

--- simple.py
+++ simple.py
@@ -4,6 +4,8 @@ import glob
 import unicodedata
 import string
 import torch
+import torch.nn as nn
+from torch.autograd import Variable
 
 
 def findFiles(path):
@@ -65,3 +67,31 @@ def lineToTensor(line):
 print(letterToTensor('J'))
 
 print(lineToTensor('Jones').size())
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax()
+
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return hidden, output
+
+
+    def initHidden(self):
+        return Variable(torch.zeros(1, self.hidden_size))
+
+
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, n_categories)
+
