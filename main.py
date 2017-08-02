c37354d454d061fd408f447cb348cc2a9c771013
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2017-08-02 22:54:57 +0200

    swap names

--- main.py
+++ main.py
@@ -1,310 +1,111 @@
-from poseLSTM import PoseLSTM
-from dataimport import KITTI, Dummy
-from torchvision import models, transforms
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from PIL import Image
-import torch
-import torch.nn as nn
 import argparse
 import os
-import plots
+import torch
 import time
 import shutil
-import numpy as np
+from base import BaseExperiment
+from binarypose import BinaryPoseCNN
+
+
+OUT_ROOT_FOLDER = 'out'
+ARCHS = {
+    # 'cnnlstm': BaseExperiment,
+    'binarypose': BinaryPoseCNN,
+}
 
 
 def setup_environment():
-    os.makedirs(out_base_folder, exist_ok=True)
+    os.makedirs(OUT_ROOT_FOLDER, exist_ok=True)
+
+
+def reset_folder(folder):
     # Wipe all existing data
-    if os.path.isdir(out_folder):
-        shutil.rmtree(out_folder)
-
-    os.makedirs(out_folder)
-
-
-def main():
-    sequence_length = args.sequence
-
-    # Image pre-processing
-    transform = get_transform(normalize=False)
-
-    kitti_train = KITTI.Subsequence(sequence_length, transform, args.grayscale,
-                                    sequence_numbers=KITTI.SEQUENCES['training'])
-
-    kitti_val = KITTI.Subsequence(sequence_length, transform, args.grayscale,
-                                  sequence_numbers=KITTI.SEQUENCES['validation'])
-
-    kitti_test = KITTI.Subsequence(sequence_length, transform, args.grayscale,
-                                   sequence_numbers=KITTI.SEQUENCES['test'])
-
-    image_size = kitti_train[0][0].size()[1:4]
-    print('Image size:', image_size)
-
-    dataloader_train = DataLoader(kitti_train, batch_size=1,
-                                  shuffle=True, num_workers=args.workers)
-
-    dataloader_val = DataLoader(kitti_val, batch_size=1,
-                                shuffle=False, num_workers=args.workers)
-
-    dataloader_test = DataLoader(kitti_test, batch_size=1,
-                                 shuffle=False, num_workers=args.workers)
-
-    # VGG without classifier
-    # Input tensor dimensions: [batch, channels, height, width]
-    # Output tensor dimensions: [batch, channels2, height2, width2]
-    vgg = models.vgg19(pretrained=True).features
-    # Freeze params, no gradient computation required
-    for param in vgg.parameters():
-        param.requires_grad = False
-
-    if use_cuda:
-        print('Moving CNN to GPU ...')
-        vgg.cuda()
-
-    # LSTM to predict pose sequence
-    # Input size to LSTM is determined by output of pre-CNN
-    print('Determine output size of CNN ...')
-    input_size = cnn_feature_size(vgg, image_size[1], image_size[2])
-    lstm = PoseLSTM(input_size=input_size, hidden_size=args.hidden_size, num_layers=args.layers)
-
-    # Loss and Optimizer
-    criterion = nn.MSELoss()
-
-    if use_cuda:
-        print('Moving LSTM to GPU ...')
-        lstm.cuda()
-        criterion.cuda()
-
-    optimizer = torch.optim.Adam(params=lstm.get_parameters(), lr=args.learning_rate)
-
-    # Train the model
-    print('Training ...')
-
-    start_time = time.time()
-    train_loss = []
-    validation_loss = []
-
-    for epoch in range(args.epochs):
-
-        # Train for one epoch
-        epoch_loss = train(vgg, lstm, criterion, optimizer, dataloader_train, epoch)
-        train_loss.append(epoch_loss)
-
-        # Validate after each epoch
-        epoch_loss = validate(vgg, lstm, criterion, dataloader_val)
-        validation_loss.append(epoch_loss)
-
-        # TODO: write validation loss
-        with open(loss_file, 'a') as f:
-            f.write('{}\n'.format(epoch_loss))
-
-        # TODO: save loses
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'train_loss': None,
-            'validation_loss': None,
-            'state_dict': lstm.state_dict(),
-            'optimizer': optimizer.state_dict(),
-        }, save_model_name)
-
-        plots.plot_epoch_loss(train_loss, validation_loss, save=save_loss_plot)
-
-    elapsed_time = time.time() - start_time
-    print('Done. Elapsed time: {:.4f} hours.'.format(elapsed_time / 3600))
-
-    # Evaluate model on testset
-    print('Testing ...')
-    test_loss = test(vgg, lstm, criterion, dataloader_test)
-    print('Done. Loss on testset: {:.4f}'.format(test_loss))
-
-
-def train(pre_cnn, lstm, criterion, optimizer, dataloader, epoch):
-    epoch_loss = 0
-    num_train_samples = len(dataloader)
-
-    for i, (images, poses) in enumerate(dataloader):
-
-        # Remove singleton batch dimension from data loader
-        images.squeeze_(0)
-
-        # Reshape target pose from [batch, 1, 6] to [1, sequence_length, 6]
-        # poses = poses.permute(1, 0, 2)
-
-        cnn_output = apply_cnn_to_sequence(pre_cnn, images, batch_mode=args.cnn_mode)
-        lstm_input = to_variable(reshape_cnn_output(cnn_output))
-        lstm_target = to_variable(poses)
-
-        #print('Input sequence to LSTM', lstm_input.size())
-        #print('Target sequence to LSTM', lstm_target.size())
-
-        lstm.zero_grad()
-        lstm_output, lstm_hidden = lstm(lstm_input)
-
-        #print('LSTM output size:', lstm_output.size())
-        #print('LSTM hidden size:', lstm_hidden[0].size())
-        #print('LSTM state size:', lstm_hidden[1].size())
-
-        # print('Output:', lstm_output)
-        # print('Target:', lstm_target)
-
-        loss = criterion(lstm_output, lstm_target)
-        loss.backward()
-        optimizer.step()
-
-        # Print log info
-        if (i + 1) % args.print_freq == 0:
-            print('Epoch [{:d}/{:d}], Sample [{:d}/{:d}], Loss: {:.4f}'
-                  .format(epoch + 1, args.epochs, i + 1, num_train_samples, loss.data[0]))
-
-        epoch_loss += loss.data[0]
-
-    epoch_loss /= num_train_samples
-    return epoch_loss
-
-
-def validate(pre_cnn, lstm, criterion, dataloader):
-    return test(pre_cnn, lstm, criterion, dataloader)
-
-
-def test(pre_cnn, lstm, criterion, dataloader):
-    avg_loss = 0
-    for i, (images, poses) in enumerate(dataloader):
-        images.squeeze_(0)
-        cnn_output = apply_cnn_to_sequence(pre_cnn, images, batch_mode=args.cnn_mode)
-        lstm_input = to_variable(reshape_cnn_output(cnn_output), volatile=True)
-        lstm_target = to_variable(poses, volatile=True)
-
-        lstm.zero_grad()
-        lstm_output, lstm_hidden = lstm(lstm_input)
-        loss = criterion(lstm_output, lstm_target)
-        avg_loss += loss.data[0]
-
-    avg_loss /= len(dataloader)
-    return avg_loss
-
-
-def apply_cnn_to_sequence(cnn, images, batch_mode=True):
-
-    if batch_mode:
-        #print('Forward sequence using CNN in batch mode.')
-        #print('Input size:', images.size())
-        features = cnn(to_variable(images, volatile=True))
-        return features.data
-    else:
-        #print('Forward sequence using CNN sequentially.')
-
-        # Transform all images in the sequence to features for the LSTM
-        batch_size = images.size(0)
-        input_sequence = []
-        for i in range(0, batch_size):
-            input = to_variable(images[i, :].unsqueeze(0), volatile=True)
-            print(input.size())
-            output = cnn(input)
-            input_sequence.append(output.data)
-
-        features = torch.cat(tuple(input_sequence), 0)
-        # Shape: [sequence length, channels, height, width]
-        return features
-
-
-def reshape_cnn_output(cnn_output):
-    sequence_lenth = cnn_output.size(0)
-    feature_size = cnn_output.size(1) * cnn_output.size(2) * cnn_output.size(3)
-    return cnn_output.view(1, sequence_lenth, feature_size)
-
-
-def cnn_feature_size(cnn, input_height, input_width):
-    input = to_variable(torch.zeros(1, 3, input_height, input_width), volatile=True)
-    output = cnn(input)
-    return output.size(1) * output.size(2) * output.size(3)
-
-
-def to_variable(data, volatile=False):
-    var = Variable(data, volatile=volatile)
-    if use_cuda:
-        var = var.cuda()
-    return var
-
-
-def display_torch_image(img):
-    tf = transforms.ToPILImage()
-    img = tf(img)
-    img.show()
-
-
-def save_checkpoint(state, filename):
-    torch.save(state, filename)
-
-
-def get_transform(normalize=False):
-    transform_list = []
-    # Image resize
-    if args.image_size:
-        transform_list.append(transforms.Scale(args.image_size))
-    # Converts image to tensor with values in range [0, 1]
-    transform_list.append(transforms.ToTensor())
-    # For normalization, see https://github.com/pytorch/vision#models
-    if normalize:
-        transform_list.append(transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                   std=[0.229, 0.224, 0.225]))
-
-    return transforms.Compose(transform_list)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    if os.path.isdir(folder):
+        shutil.rmtree(folder)
+    os.makedirs(folder)
+
+
+def get_main_parser():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--resume', action='store_true')
 
     parser.add_argument('--cuda', action='store_true')
-
-    parser.add_argument('--cnn_mode', type=int, choices=[0, 1], default=1,
-                        help='0: Sequential mode 1: Batch mode')
 
     parser.add_argument('--print_freq', type=int, default=100,
                         help='Frequency of printed information during training')
 
-    parser.add_argument('--experiment', type=str, default='unnamed',
+    parser.add_argument('--name', type=str, default='unnamed',
                         help='Name of the experiment. Output files will be stored in this folder.')
-
-    # Model parameters
-    parser.add_argument('--hidden_size', type=int, default=1000,
-                        help='dimension of lstm hidden states')
-    parser.add_argument('--layers', type=int, default=1,
-                        help='number of layers in LSTM')
-
-    # Data loading parameters
-    parser.add_argument('--sequence', type=int, default=10,
-                        help='Sequence length')
-
-    parser.add_argument('--image_size', type=int, default=None,
-                        help='Input images will be scaled such that the shorter side is equal to the given value.')
-
-    parser.add_argument('--grayscale', action='store_true',
-                        help='Convert images to grayscale.')
 
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--workers', type=int, default=2)
 
     # Training parameters
-    parser.add_argument('--epochs', type=int, default=5)
-    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=1)
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='Learning rate')
+    return parser
 
-    args = parser.parse_args()
-    print(args)
 
-    out_base_folder = 'out'
-    out_folder = os.path.join(out_base_folder, args.experiment)
-    loss_file = os.path.join(out_folder, 'loss.txt')
-    save_loss_plot = os.path.join(out_folder, 'loss.pdf')
-    save_model_name = os.path.join(out_folder, 'checkpoint.pth.tar')
-
-    use_cuda = torch.cuda.is_available() and args.cuda
-
+def print_cuda_status():
     if torch.cuda.is_available():
         print('CUDA is available on this machine.')
     else:
         print('CUDA is not available on this machine.')
 
+
+def print_elapsed_hours(elapsed):
+    print('Elapsed time: {:.4f} hours.'.format(elapsed / 3600))
+
+
+if __name__ == '__main__':
+    print_cuda_status()
     setup_environment()
-    main()
-    
+
+    main_parser = get_main_parser()
+    subparsers = main_parser.add_subparsers()
+
+    for name, c in ARCHS.items():
+        sub = subparsers.add_parser(name, parents=[main_parser])
+        c.submit_arguments(sub)
+        sub.set_defaults(create=c)
+
+    args = main_parser.parse_args()
+    print(args)
+
+    experiment_folder = os.path.join(OUT_ROOT_FOLDER, args.name)
+    if args.train and not args.resume:
+        reset_folder(experiment_folder)
+
+    experiment = args.create(experiment_folder, args)
+
+    start_epoch = 1
+    checkpoint = None
+    if args.resume:
+        print('Loading checkpoint ...')
+        checkpoint = experiment.load_checkpoint()
+        experiment.restore_from_checkpoint(checkpoint)
+        start_epoch = checkpoint['epoch'] + 1
+
+    if args.train:
+        print('Training for {} epochs ...'.format(args.epochs))
+        start_time = time.time()
+
+        for epoch in range(start_epoch, start_epoch + args.epochs):
+            # Train for one epoch
+            print('Epoch [{:d}/{:d}]'.format(epoch, start_epoch + args.epochs - 1))
+            experiment.train()
+            experiment.save_checkpoint(experiment.make_checkpoint())
+
+        experiment.plot_performance()
+        print(print_elapsed_hours(time.time() - start_time))
+
+    if args.test:
+        print('Testing ...')
+        start_time = time.time()
+        checkpoint = experiment.load_checkpoint()
+        experiment.restore_from_checkpoint(checkpoint)
+        experiment.test()
+        print(print_elapsed_hours(time.time() - start_time))
