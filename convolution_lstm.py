ac00dee16ee0528d7f927af1a7aaee5804436c6a
awaelchli <adrian.waelchli@students.unibe.ch>
2017-08-07 18:44:19 +0200

    modified original forward to support multiple input

--- convolution_lstm.py
+++ convolution_lstm.py
@@ -42,15 +42,13 @@ class ConvLSTMCell(nn.Module):
 class ConvLSTM(nn.Module):
     # input_channels corresponds to the first input feature map
     # hidden state is a list of succeeding lstm layers.
-    def __init__(self, input_channels, hidden_channels, kernel_size, step=1, effective_step=[1], bias=True):
+    def __init__(self, input_channels, hidden_channels, kernel_size, bias=True):
         super(ConvLSTM, self).__init__()
         self.input_channels = [input_channels] + hidden_channels
         self.hidden_channels = hidden_channels
         self.kernel_size = kernel_size
         self.num_layers = len(hidden_channels)
-        self.step = step
         self.bias = bias
-        self.effective_step = effective_step
         self._all_layers = []
         for i in range(self.num_layers):
             name = 'cell{}'.format(i)
@@ -58,27 +56,26 @@ class ConvLSTM(nn.Module):
             setattr(self, name, cell)
             self._all_layers.append(cell)
 
-    def forward(self, input):
+    def forward(self, input, internal_state=None):
         internal_state = []
-        outputs = []
-        for step in range(self.step):
-            x = input
+        if not internal_state:
+            # First forward pass in the time sequence
+            # Initialize internal states of all layers
             for i in range(self.num_layers):
-                # all cells are initialized in the first step
-                if step == 0:
-                    bsize, _, height, width = x.size()
-                    (h, c) = ConvLSTMCell.init_hidden(bsize, self.hidden_channels[i], (height, width))
-                    internal_state.append((h, c))
-                # do forward
-                name = 'cell{}'.format(i)
-                (h, c) = internal_state[i]
+                bsize, _, height, width = input.size()
+                (h, c) = ConvLSTMCell.init_hidden(bsize, self.hidden_channels[i], (height, width))
+                if input.is_cuda:
+                    h = h.cuda()
+                    c = c.cuda()
+                internal_state.append((h, c))
 
-                x, new_c = getattr(self, name)(x, h, c)
-                internal_state[i] = (x, new_c)
-            # only record effective steps
-            if step in self.effective_step:
-                outputs.append(x)
+        # Forward pass through all layers in current time step
+        x = input
+        for i in range(self.num_layers):
+            name = 'cell{}'.format(i)
+            (h, c) = internal_state[i]
 
-        return outputs, (x, new_c)
+            x, new_c = getattr(self, name)(x, h, c)
+            internal_state[i] = (x, new_c)
 
-
+        return x, internal_state
