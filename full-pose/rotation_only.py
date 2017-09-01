ae43a021b5bf2d99e71bd4a8a9a65bf2f2bb3b55
awaelchli <adrian.waelchli@students.unibe.ch>
2017-09-01 13:37:40 +0200

    data

--- full-pose/rotation_only.py
+++ full-pose/rotation_only.py
@@ -288,7 +288,7 @@ class RotationOnly(BaseExperiment):
                 print('Flow: ', flow)
                 filename = '{}/{}.jpg'.format(i, j)
                 f = self.make_output_filename(filename)
-                save_image(flow, f)
+                save_image(flow.data, f)
 
     def validate(self):
         avg_loss = AverageMeter()
