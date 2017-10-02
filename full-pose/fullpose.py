638c97382f6383280c5e80051732c2b052a0cc70
awaelchli <adrian.waelchli@students.unibe.ch>
2017-10-02 18:19:40 +0200

    more stride

--- full-pose/fullpose.py
+++ full-pose/fullpose.py
@@ -38,16 +38,16 @@ class FullPose7DModel(nn.Module):
         # )
 
         self.layers = torch.nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=0, dilation=2),
+            nn.Conv2d(3, 64, kernel_size=5, stride=5, padding=0, dilation=2),
             nn.LeakyReLU(0.1),
 
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=0, dilation=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=3, padding=0, dilation=2),
             nn.LeakyReLU(0.1),
 
-            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=0, dilation=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=3, padding=0, dilation=2),
             nn.LeakyReLU(0.1),
 
-            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=0, dilation=2),
+            nn.Conv2d(256, 512, kernel_size=3, stride=3, padding=0, dilation=2),
             nn.LeakyReLU(0.1),
         )
 
