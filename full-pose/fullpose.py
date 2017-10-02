dc330217e084195cce213ab20c6d18b160f74cc4
awaelchli <adrian.waelchli@students.unibe.ch>
2017-10-02 18:32:22 +0200

    fine stride

--- full-pose/fullpose.py
+++ full-pose/fullpose.py
@@ -38,7 +38,7 @@ class FullPose7DModel(nn.Module):
         # )
 
         self.layers = torch.nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=5, stride=5, padding=0, dilation=2),
+            nn.Conv2d(3, 64, kernel_size=5, stride=3, padding=0, dilation=2),
             nn.LeakyReLU(0.1),
 
             nn.Conv2d(64, 128, kernel_size=3, stride=3, padding=0, dilation=2),
@@ -210,7 +210,7 @@ class FullPose7D(BaseExperiment):
         #seq_transform = None
         seq_transform = transforms.Compose([
             #RandomSequenceReversal(),
-            Loop(8, 12),
+            Loop(self.sequence_length - 10, self.sequence_length + 10),
         ])
 
         zipped = True
