1dac5b1cc618b80030443871f98bc4c0b7e7001b
awaelchli <adrian.waelchli@students.unibe.ch>
2017-10-03 13:45:42 +0200

    cnn

--- dil/fullpose.py
+++ dil/fullpose.py
@@ -45,8 +45,8 @@ class FullPose7DModel(nn.Module):
             nn.Conv2d(64, 128, kernel_size=3, stride=3, padding=0, dilation=2),
             nn.LeakyReLU(0.1),
 
-            #nn.Conv2d(128, 256, kernel_size=3, stride=3, padding=0, dilation=2),
-            #nn.LeakyReLU(0.1),
+            nn.Conv2d(128, 256, kernel_size=3, stride=3, padding=0, dilation=2),
+            nn.LeakyReLU(0.1),
 
             #nn.Conv2d(256, 512, kernel_size=3, stride=3, padding=0, dilation=2),
             #nn.LeakyReLU(0.1),
