b2cdcaec94e993c6d414aefeaf19b51ed7a61e20
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2017-10-23 16:35:31 +0200

    average meter moved to utils

---
+++ utils/training.py
@@ -0,0 +1,16 @@
+class AverageMeter(object):
+    """ Computes and stores the average and current value """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.sum = 0
+        self.count = 0
+
+    @property
+    def average(self):
+        return self.sum / self.count
+
+    def update(self, value):
+        self.sum += value
+        self.count += 1
