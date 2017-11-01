e06ec234e36a38c81f3b3bc042819949fa6e8d53
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2017-11-01 18:06:40 +0100

    refactor

--- utils/GTAV.py
+++ utils/GTAV.py
@@ -1,6 +1,6 @@
 import glob
 import os
-from math import radians, ceil
+from math import radians
 from os import path
 
 import numpy as np
@@ -9,8 +9,8 @@ from PIL import Image
 from scipy import interpolate
 from torch.utils.data import Dataset
 from transforms3d.euler import euler2quat
+
 from pose_transforms import relative_to_first_pose
-from torch.utils.data.sampler import SequentialSampler
 
 FOLDERS = {
     'walking': {
@@ -45,34 +45,6 @@ FOLDERS = {
 
 IMAGE_EXTENSION = 'jpg'
 POSE_FILE_EXTENSION = 'txt'
-
-
-class StridedSampler(SequentialSampler):
-    """ Samples elements sequentially, always in the same order, but with a given stride (skipping elements).
-
-    Arguments:
-        data_source (Dataset): dataset to sample from
-        stride (int): stride to apply when sampling
-        max_size (int): clips the dataset such that there are at most max_size samples
-    """
-
-    def __init__(self, data_source, stride=1, max_size=None):
-        super(StridedSampler, self).__init__(data_source)
-        self.stride = stride
-        self.max_size = max_size
-        self.indices = self.get_indices()
-
-    def __iter__(self):
-        return iter(self.indices)
-
-    def __len__(self):
-        return len(self.indices)
-
-    def get_indices(self):
-        indices = range(0, len(self.data_source), self.stride)
-        if self.max_size:
-            indices = indices[:self.max_size]
-        return indices
 
 
 class SequenceCollection(object):
